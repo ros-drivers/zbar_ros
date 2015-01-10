aca05f8fef215dcc1d21a9818f29fb40f163e92e
Paul Bovbel <pbovbel@clearpathrobotics.com>
2015-01-10 15:33:31 -0500

    pre release cleanup

--- include/zbar_ros/zbar_ros_base.h
+++ include/zbar_ros/zbar_ros_base.h
@@ -1,11 +1,43 @@
-#ifndef ZBAR_ROS_BASE_H
-#define ZBAR_ROS_BASE_H
+/**
+*
+*  \author     Paul Bovbel <pbovbel@clearpathrobotics.com>
+*  \copyright  Copyright (c) 2014, Clearpath Robotics, Inc.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of Clearpath Robotics, Inc. nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL CLEARPATH ROBOTICS, INC. BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Please send comments, questions, or patches to code@clearpathrobotics.com
+*
+*/
 
-#include <ros/ros.h>
-#include <cv.h>
-#include <cv_bridge/cv_bridge.h>
-#include <zbar.h>
-#include <tf/transform_listener.h>
+#ifndef ZBAR_ROS_ZBAR_ROS_BASE_H
+#define ZBAR_ROS_ZBAR_ROS_BASE_H
+
+#include "ros/ros.h"
+#include "opencv/cv.h"
+#include "cv_bridge/cv_bridge.h"
+#include "zbar.h"
+#include "boost/unordered_map.hpp"
+#include <string>
 
 namespace zbar_ros
 {
@@ -13,26 +45,23 @@ namespace zbar_ros
   class ZbarBase
   {
   public:
-
     ZbarBase(ros::NodeHandle nh, ros::NodeHandle private_nh);
 
   private:
-
     void connectCb();
 
     void disconnectCb();
 
     void imageCb(const sensor_msgs::ImageConstPtr &image);
 
-
     ros::NodeHandle nh_, private_nh_;
-    tf::TransformListener tf_;
     ros::Subscriber camera_sub_;
     ros::Publisher barcode_pub_;
     zbar::ImageScanner scanner_;
+    boost::unordered_map<std::string, ros::Time> barcode_memory_;
 
+    double throttle_;
   };
+}  // namespace zbar_ros
 
-}
-
-#endif  // ZBAR_ROS_BASE_H
+#endif  // ZBAR_ROS_ZBAR_ROS_BASE_H
